9abc2cbc103e7a308f39838a92cbade98303a47c
Jaroslav Vondrák <jaroslavvondrak12@seznam.cz>
2020-05-13 00:04:45 +0200

    upload
    přidání upload, příprava download

--- src/main/java/com/weather/weather/rest/RestControl.java
+++ src/main/java/com/weather/weather/rest/RestControl.java
@@ -3,12 +3,18 @@ package com.weather.weather.rest;
 
 import com.weather.weather.Main;
 import com.weather.weather.configurations.ConfigProperties;
+import com.weather.weather.configurations.FileStorageProperties;
 import com.weather.weather.view.MainPage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 public class RestControl {
@@ -17,6 +23,8 @@ public class RestControl {
     MainPage mainPage;
     @Autowired
     ConfigProperties configProperties;
+    @Autowired
+    FileStorageProperties fileStorageProperties;
 
    @GetMapping("/")
     String GetStateInfo() {
@@ -31,6 +39,41 @@ public class RestControl {
     @GetMapping("/week2")
     String getWeek2Info() {
         return mainPage.ShowMainPage(2);
+    }
+
+    @PostMapping("/city/import")
+    public void uploadFile(HttpServletResponse response, @RequestParam("file") MultipartFile file) {
+
+       /* if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }*/
+
+        try {
+
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(fileStorageProperties.getUploadDir() + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            /*redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");*/
+
+            response.sendRedirect("/");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping("/city/export/{filename:.+}")
+    public void downloadFile(@PathVariable String filename) {
+
+       //export dat z databáze
+
+        File file = new File(fileStorageProperties.getDownloadDir() + "/" + filename);
+
+
     }
 
     @PostMapping("/state/change")
